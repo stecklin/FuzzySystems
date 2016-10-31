417b975617f5593232804dce133ad75612efa82d
Marianne Stecklina <marianne-stecklina@gmx.de>
2016-10-31 16:33:39 +0100

    Designed structure

---
+++ alphacuts/alphacuts.py
@@ -0,0 +1,20 @@
+class FuzzySet(object):
+    """
+    Horizontal representation of a fuzzy set by its alpha cuts.
+    """
+
+    def __init__(self, alpha_levels):
+        """Initialize fuzzy set with a list of alpha levels."""
+        pass
+
+    def get_alpha_levels(self):
+        """Return list of alpha levels."""
+        pass
+
+    def set_alpha_cut(self, alpha_level, alpha_cut):
+        """Set alpha cut for a specific alpha level."""
+        pass
+
+    def membership_degree(self, x):
+        """Return the membership degree for element x."""
+        pass
---
+++ assignment1.py
@@ -0,0 +1,3 @@
+# subtask a) Read alpha levels
+# subtask b) Read alpha cuts for each level
+# subtask c) Return membership degree for an element x
