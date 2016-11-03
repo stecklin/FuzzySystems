2d436ee9ea2e52ec1c99ef536a546758c6fe9b79
Marianne Stecklina <marianne-stecklina@gmx.de>
2016-11-03 23:09:52 +0100

    Finished assignment 1 script

--- alphacuts/alphacuts.py
+++ alphacuts/alphacuts.py
@@ -11,12 +11,13 @@ class FuzzySet(object):
         # check if levels are between 0 and 1
         for level in alpha_levels:
             if not 0 <= level <= 1:
-                raise ValueError
+                raise ValueError('Levels must be between 0 and 1.')
 
         alpha_levels.sort(reverse=True)
-        self.alpha_cuts = OrderedDict({level: [] for level in alpha_levels})
+        self.alpha_cuts = OrderedDict((level, []) for level in alpha_levels)
 
     def _contains(self, alpha_list, x):
+        """Check whether x is contained in alpha_list."""
         for element in alpha_list:
             if element[0] <= x <= element[1]:
                 return True
@@ -30,9 +31,13 @@ class FuzzySet(object):
         """Set alpha cut for a specific alpha level."""
         self.alpha_cuts[alpha_level] = alpha_cut
 
+    def append_alpha_cut(self, alpha_level, set_tuple):
+        """Append a single tuple to a specific alpha level."""
+        self.alpha_cuts[alpha_level].append(set_tuple)
+
     def membership_degree(self, x):
         """Return the membership degree for element x."""
         for alpha_level in self.alpha_cuts:
             if self._contains(self.alpha_cuts[alpha_level], x):
                 return alpha_level
-        return 0
+        raise ValueError('Element out of universe of discourse.')
--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,21 @@
+from alphacuts.alphacuts import FuzzySet
+
+
 # subtask a) Read alpha levels
+alpha_levels = raw_input('Please enter alpha levels (e.g. 0 0.5 1): ')
+alpha_level_list = [float(level) for level in alpha_levels.split(' ')]
+fuzzy_set = FuzzySet(alpha_level_list)
+
 # subtask b) Read alpha cuts for each level
+for level in fuzzy_set.get_alpha_levels():
+    alpha_cut = raw_input('Please enter alpha cut for level ' + str(level) + ' (e.g. 5-7 10-12): ')
+    alpha_cut_list = alpha_cut.split(' ')
+
+    for set_tuple in alpha_cut_list:
+        lower_bound, upper_bound = set_tuple.split('-')
+        fuzzy_set.append_alpha_cut(level, (float(lower_bound), float(upper_bound)))
+
 # subtask c) Return membership degree for an element x
+while True:
+    x = raw_input('Please enter an element to request its membership: ')
+    print fuzzy_set.membership_degree(float(x))
